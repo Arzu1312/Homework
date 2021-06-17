94ac9e52ce3831709f35581688751a16d12b33b4
Arzu <arzu.shahbazli@bk.ru>
2021-06-18 01:49:37 +0400

    exam-table

---
+++ JS/ders2/main.js
@@ -0,0 +1,345 @@
+const dateInfo = '14 iyun';
+const timeInfo = 18.30;
+const studentInfo= 18.15;
+
+if (dateInfo == '1 iyun') {
+    console.log("Qerar qebulunu destekleyen sistemler");
+    if (timeInfo>=17 && timeInfo <= 18) {
+        console.log("Arzu Sahbazli "  ,  " Jale Eliyeva" ,  " Sultan Kerimova" , " Qadir Ceferov" , " Cemil Ibisov" , " Kamran Kerimzade");
+        if (studentInfo>=17 && studentInfo<=17.10) {
+            console.log("Arzu Sahbazli");
+        }else if (studentInfo>17.10 && studentInfo<=17.20) {
+            console.log("Jale Eliyeva");
+        }else if  (studentInfo>17.20 && studentInfo<=17.30) {
+            console.log("Sultan Kerimova");
+        }else if  (studentInfo>17.30 && studentInfo<=17.40) {
+            console.log("Qadir Ceferov");
+        }else if  (studentInfo>17.40 && studentInfo<=17.50) {
+            console.log("Cemil Ibisov");
+        }else {
+            console.log("Kamran Kerimzade");
+        }
+
+    }else if (timeInfo>18 && timeInfo <= 19){
+        console.log("Leman Dadasova "  ,  " Saida Abiyeva" ,  " Cavid Quliyev" , " Sebine Mailova" , " Isaq Veliyev" , " Qara Abdullayev");
+        if (studentInfo>18 && studentInfo<=18.10) {
+            console.log("Leman Dadasova");
+        }else if (studentInfo>18.10 && studentInfo<=18.20) {
+            console.log("Saida Abiyeva");
+        }else if  (studentInfo>18.20 && studentInfo<=18.30) {
+            console.log("Cavid Quliyev");
+        }else if  (studentInfo>18.30 && studentInfo<=18.40) {
+            console.log("Sebine Msilova");
+        }else if  (studentInfo>18.40 && studentInfo<=18.50) {
+            console.log("Isaq Veliyev");
+        }else {
+            console.log("Qara Abdullayev");
+        }
+
+    }else if (timeInfo>19 && timeInfo <= 20) {
+        console.log("Gulnar Veliyev "  ,  " Vusal Memmedov" ,  " Ramil Memmedov" , " Zahid Memmedli" , " Burhan Ehmedov" , " Museddin Kerimov");
+        if (studentInfo>19 && studentInfo<=19.10) {
+            console.log("Gulnar Veliyev");
+        }else if (studentInfo>19.10 && studentInfo<=19.20) {
+            console.log("Vusal Memmedov");
+        }else if  (studentInfo>19.20 && studentInfo<=19.30) {
+            console.log("Ramil Memmedov");
+        }else if  (studentInfo>19.30 && studentInfo<=19.40) {
+            console.log("Zahid Memmedli");
+        }else if  (studentInfo>19.40 && studentInfo<=19.50) {
+            console.log("Burhan Ehmedov");
+        }else {
+            console.log("Museddin Kerimov");
+        }
+
+    }else {
+        console.log("Qismet Eliyev "  ,  " Togrul Rzayev" ,  " Abbas Abbasov" , " Orxan Ibrahimov" , " Eldar Novruzov" , " Emrah Eyyubov");
+        if (studentInfo>20 && studentInfo<=20.10) {
+            console.log("Qismet Eliyev");
+        }else if (studentInfo>20.10 && studentInfo<=20.20) {
+            console.log("Togrul Rzayev");
+        }else if  (studentInfo>20.20 && studentInfo<=20.30) {
+            console.log("Abbas Abbasov");
+        }else if  (studentInfo>20.30 && studentInfo<=20.40) {
+            console.log("Orxan Ibrahimov");
+        }else if  (studentInfo>20.40 && studentInfo<=20.50) {
+            console.log("Eldar Novruzov");
+        }else {
+            console.log("Emrah Eyyubov");
+        }
+    }
+
+}else if (dateInfo == '7 iyun') {
+    console.log("informasiya texnologiyalari ve sistemleri muhendisliyinin muasir problemleri");
+    if (timeInfo>=17 && timeInfo <= 18) {
+        console.log("Arzu Sahbazli "  ,  " Jale Eliyeva" ,  " Sultan Kerimova" , " Qadir Ceferov" , " Cemil Ibisov" , " Kamran Kerimzade");
+        if (studentInfo>=17 && studentInfo<=17.10) {
+            console.log("Arzu Sahbazli");
+        }else if (studentInfo>17.10 && studentInfo<=17.20) {
+            console.log("Jale Eliyeva");
+        }else if  (studentInfo>17.20 && studentInfo<=17.30) {
+            console.log("Sultan Kerimova");
+        }else if  (studentInfo>17.30 && studentInfo<=17.40) {
+            console.log("Qadir Ceferov");
+        }else if  (studentInfo>17.40 && studentInfo<=17.50) {
+            console.log("Cemil Ibisov");
+        }else {
+            console.log("Kamran Kerimzade");
+        }
+    }else if (timeInfo>18 && timeInfo <= 19){
+        console.log("Leman Dadasova "  ,  " Saida Abiyeva" ,  " Cavid Quliyev" , " Sebine Mailova" , " Isaq Veliyev" , " Qara Abdullayev");
+        if (studentInfo>18 && studentInfo<=18.10) {
+            console.log("Leman Dadasova");
+        }else if (studentInfo>18.10 && studentInfo<=18.20) {
+            console.log("Saida Abiyeva");
+        }else if  (studentInfo>18.20 && studentInfo<=18.30) {
+            console.log("Cavid Quliyev");
+        }else if  (studentInfo>18.30 && studentInfo<=18.40) {
+            console.log("Sebine Msilova");
+        }else if  (studentInfo>18.40 && studentInfo<=18.50) {
+            console.log("Isaq Veliyev");
+        }else {
+            console.log("Qara Abdullayev");
+        }
+    }else if (timeInfo>19 && timeInfo <= 20) {
+        console.log("Gulnar Veliyev "  ,  " Vusal Memmedov" ,  " Ramil Memmedov" , " Zahid Memmedli" , " Burhan Ehmedov" , " Museddin Kerimov");
+        if (studentInfo>19 && studentInfo<=19.10) {
+            console.log("Gulnar Veliyev");
+        }else if (studentInfo>19.10 && studentInfo<=19.20) {
+            console.log("Vusal Memmedov");
+        }else if  (studentInfo>19.20 && studentInfo<=19.30) {
+            console.log("Ramil Memmedov");
+        }else if  (studentInfo>19.30 && studentInfo<=19.40) {
+            console.log("Zahid Memmedli");
+        }else if  (studentInfo>19.40 && studentInfo<=19.50) {
+            console.log("Burhan Ehmedov");
+        }else {
+            console.log("Museddin Kerimov");
+        }
+    }else {
+        console.log("Qismet Eliyev "  ,  " Togrul Rzayev" ,  " Abbas Abbasov" , " Orxan Ibrahimov" , " Eldar Novruzov" , " Emrah Eyyubov");
+        if (studentInfo>20 && studentInfo<=20.10) {
+            console.log("Qismet Eliyev");
+        }else if (studentInfo>20.10 && studentInfo<=20.20) {
+            console.log("Togrul Rzayev");
+        }else if  (studentInfo>20.20 && studentInfo<=20.30) {
+            console.log("Abbas Abbasov");
+        }else if  (studentInfo>20.30 && studentInfo<=20.40) {
+            console.log("Orxan Ibrahimov");
+        }else if  (studentInfo>20.40 && studentInfo<=20.50) {
+            console.log("Eldar Novruzov");
+        }else {
+            console.log("Emrah Eyyubov");
+        }
+    }
+
+}else if (dateInfo == '11 iyun') {
+    console.log("Korporativ sebekeler");
+    if (timeInfo>=17 && timeInfo <= 18) {
+        console.log("Arzu Sahbazli "  ,  " Jale Eliyeva" ,  " Sultan Kerimova" , " Qadir Ceferov" , " Cemil Ibisov" , " Kamran Kerimzade");
+        if (studentInfo>=17 && studentInfo<=17.10) {
+            console.log("Arzu Sahbazli");
+        }else if (studentInfo>17.10 && studentInfo<=17.20) {
+            console.log("Jale Eliyeva");
+        }else if  (studentInfo>17.20 && studentInfo<=17.30) {
+            console.log("Sultan Kerimova");
+        }else if  (studentInfo>17.30 && studentInfo<=17.40) {
+            console.log("Qadir Ceferov");
+        }else if  (studentInfo>17.40 && studentInfo<=17.50) {
+            console.log("Cemil Ibisov");
+        }else {
+            console.log("Kamran Kerimzade");
+        }
+    }else if (timeInfo>18 && timeInfo <= 19){
+        console.log("Leman Dadasova "  ,  " Saida Abiyeva" ,  " Cavid Quliyev" , " Sebine Mailova" , " Isaq Veliyev" , " Qara Abdullayev");
+        if (studentInfo>18 && studentInfo<=18.10) {
+            console.log("Leman Dadasova");
+        }else if (studentInfo>18.10 && studentInfo<=18.20) {
+            console.log("Saida Abiyeva");
+        }else if  (studentInfo>18.20 && studentInfo<=18.30) {
+            console.log("Cavid Quliyev");
+        }else if  (studentInfo>18.30 && studentInfo<=18.40) {
+            console.log("Sebine Msilova");
+        }else if  (studentInfo>18.40 && studentInfo<=18.50) {
+            console.log("Isaq Veliyev");
+        }else {
+            console.log("Qara Abdullayev");
+        }
+        
+    }else if (timeInfo>19 && timeInfo <= 20) {
+        console.log("Gulnar Veliyev "  ,  " Vusal Memmedov" ,  " Ramil Memmedov" , " Zahid Memmedli" , " Burhan Ehmedov" , " Museddin Kerimov");
+        if (studentInfo>19 && studentInfo<=19.10) {
+            console.log("Gulnar Veliyev");
+        }else if (studentInfo>19.10 && studentInfo<=19.20) {
+            console.log("Vusal Memmedov");
+        }else if  (studentInfo>19.20 && studentInfo<=19.30) {
+            console.log("Ramil Memmedov");
+        }else if  (studentInfo>19.30 && studentInfo<=19.40) {
+            console.log("Zahid Memmedli");
+        }else if  (studentInfo>19.40 && studentInfo<=19.50) {
+            console.log("Burhan Ehmedov");
+        }else {
+            console.log("Museddin Kerimov");
+        }
+        
+    }else {
+        console.log("Qismet Eliyev "  ,  " Togrul Rzayev" ,  " Abbas Abbasov" , " Orxan Ibrahimov" , " Eldar Novruzov" , " Emrah Eyyubov");
+        if (studentInfo>20 && studentInfo<=20.10) {
+            console.log("Qismet Eliyev");
+        }else if (studentInfo>20.10 && studentInfo<=20.20) {
+            console.log("Togrul Rzayev");
+        }else if  (studentInfo>20.20 && studentInfo<=20.30) {
+            console.log("Abbas Abbasov");
+        }else if  (studentInfo>20.30 && studentInfo<=20.40) {
+            console.log("Orxan Ibrahimov");
+        }else if  (studentInfo>20.40 && studentInfo<=20.50) {
+            console.log("Eldar Novruzov");
+        }else {
+            console.log("Emrah Eyyubov");
+        }
+       
+    }
+
+}else if (dateInfo == '14 iyun') {
+    console.log("Internet/Intranet texnologiyalari");
+    if (timeInfo>=17 && timeInfo <= 18) {
+        console.log("Arzu Sahbazli "  ,  " Jale Eliyeva" ,  " Sultan Kerimova" , " Qadir Ceferov" , " Cemil Ibisov" , " Kamran Kerimzade");
+        if (studentInfo>=17 && studentInfo<=17.10) {
+            console.log("Arzu Sahbazli");
+        }else if (studentInfo>17.10 && studentInfo<=17.20) {
+            console.log("Jale Eliyeva");
+        }else if  (studentInfo>17.20 && studentInfo<=17.30) {
+            console.log("Sultan Kerimova");
+        }else if  (studentInfo>17.30 && studentInfo<=17.40) {
+            console.log("Qadir Ceferov");
+        }else if  (studentInfo>17.40 && studentInfo<=17.50) {
+            console.log("Cemil Ibisov");
+        }else {
+            console.log("Kamran Kerimzade");
+        }
+    }else if (timeInfo>18 && timeInfo <= 19){
+        console.log("Leman Dadasova "  ,  " Saida Abiyeva" ,  " Cavid Quliyev" , " Sebine Mailova" , " Isaq Veliyev" , " Qara Abdullayev");
+        if (studentInfo>18 && studentInfo<=18.10) {
+            console.log("Leman Dadasova");
+        }else if (studentInfo>18.10 && studentInfo<=18.20) {
+            console.log("Saida Abiyeva");
+        }else if  (studentInfo>18.20 && studentInfo<=18.30) {
+            console.log("Cavid Quliyev");
+        }else if  (studentInfo>18.30 && studentInfo<=18.40) {
+            console.log("Sebine Msilova");
+        }else if  (studentInfo>18.40 && studentInfo<=18.50) {
+            console.log("Isaq Veliyev");
+        }else {
+            console.log("Qara Abdullayev");
+        }
+    }else if (timeInfo>19 && timeInfo <= 20) {
+        console.log("Gulnar Veliyev "  ,  " Vusal Memmedov" ,  " Ramil Memmedov" , " Zahid Memmedli" , " Burhan Ehmedov" , " Museddin Kerimov");
+        if (studentInfo>19 && studentInfo<=19.10) {
+            console.log("Gulnar Veliyev");
+        }else if (studentInfo>19.10 && studentInfo<=19.20) {
+            console.log("Vusal Memmedov");
+        }else if  (studentInfo>19.20 && studentInfo<=19.30) {
+            console.log("Ramil Memmedov");
+        }else if  (studentInfo>19.30 && studentInfo<=19.40) {
+            console.log("Zahid Memmedli");
+        }else if  (studentInfo>19.40 && studentInfo<=19.50) {
+            console.log("Burhan Ehmedov");
+        }else {
+            console.log("Museddin Kerimov");
+        }
+       
+    }else {
+        console.log("Qismet Eliyev "  ,  " Togrul Rzayev" ,  " Abbas Abbasov" , " Orxan Ibrahimov" , " Eldar Novruzov" , " Emrah Eyyubov");
+        if (studentInfo>20 && studentInfo<=20.10) {
+            console.log("Qismet Eliyev");
+        }else if (studentInfo>20.10 && studentInfo<=20.20) {
+            console.log("Togrul Rzayev");
+        }else if  (studentInfo>20.20 && studentInfo<=20.30) {
+            console.log("Abbas Abbasov");
+        }else if  (studentInfo>20.30 && studentInfo<=20.40) {
+            console.log("Orxan Ibrahimov");
+        }else if  (studentInfo>20.40 && studentInfo<=20.50) {
+            console.log("Eldar Novruzov");
+        }else {
+            console.log("Emrah Eyyubov");
+        }
+        
+    }
+
+}else if (dateInfo == '18 iyun') {
+    console.log("Ali mekteb pedaqogikasi");
+    if (timeInfo>=17 && timeInfo <= 18) {
+        console.log("Sagolsun muellim butun qruplarin eyni gune salib");
+
+    }else{
+        console.log("Imtahan bitmisdir");
+    }
+
+}else  {
+    console.log("Tipik layihelendirme ile KIS-in qurulmasi");
+    if (timeInfo>=17 && timeInfo <= 18) {
+        console.log("Arzu Sahbazli "  ,  " Jale Eliyeva" ,  " Sultan Kerimova" , " Qadir Ceferov" , " Cemil Ibisov" , " Kamran Kerimzade");
+        if (studentInfo>=17 && studentInfo<=17.10) {
+            console.log("Arzu Sahbazli");
+        }else if (studentInfo>17.10 && studentInfo<=17.20) {
+            console.log("Jale Eliyeva");
+        }else if  (studentInfo>17.20 && studentInfo<=17.30) {
+            console.log("Sultan Kerimova");
+        }else if  (studentInfo>17.30 && studentInfo<=17.40) {
+            console.log("Qadir Ceferov");
+        }else if  (studentInfo>17.40 && studentInfo<=17.50) {
+            console.log("Cemil Ibisov");
+        }else {
+            console.log("Kamran Kerimzade");
+        }
+    }else if (timeInfo>18 && timeInfo <= 19){
+        console.log("Leman Dadasova "  ,  " Saida Abiyeva" ,  " Cavid Quliyev" , " Sebine Mailova" , " Isaq Veliyev" , " Qara Abdullayev");
+        if (studentInfo>18 && studentInfo<=18.10) {
+            console.log("Leman Dadasova");
+        }else if (studentInfo>18.10 && studentInfo<=18.20) {
+            console.log("Saida Abiyeva");
+        }else if  (studentInfo>18.20 && studentInfo<=18.30) {
+            console.log("Cavid Quliyev");
+        }else if  (studentInfo>18.30 && studentInfo<=18.40) {
+            console.log("Sebine Msilova");
+        }else if  (studentInfo>18.40 && studentInfo<=18.50) {
+            console.log("Isaq Veliyev");
+        }else {
+            console.log("Qara Abdullayev");
+        }
+    }else if (timeInfo>19 && timeInfo <= 20) {
+        console.log("Gulnar Veliyev "  ,  " Vusal Memmedov" ,  " Ramil Memmedov" , " Zahid Memmedli" , " Burhan Ehmedov" , " Museddin Kerimov");
+       
+        if (studentInfo>19 && studentInfo<=19.10) {
+            console.log("Gulnar Veliyev");
+        }else if (studentInfo>19.10 && studentInfo<=19.20) {
+            console.log("Vusal Memmedov");
+        }else if  (studentInfo>19.20 && studentInfo<=19.30) {
+            console.log("Ramil Memmedov");
+        }else if  (studentInfo>19.30 && studentInfo<=19.40) {
+            console.log("Zahid Memmedli");
+        }else if  (studentInfo>19.40 && studentInfo<=19.50) {
+            console.log("Burhan Ehmedov");
+        }else {
+            console.log("Museddin Kerimov");
+        }
+       
+    }else {
+        console.log("Qismet Eliyev "  ,  " Togrul Rzayev" ,  " Abbas Abbasov" , " Orxan Ibrahimov" , " Eldar Novruzov" , " Emrah Eyyubov");
+        if (studentInfo>20 && studentInfo<=20.10) {
+            console.log("Qismet Eliyev");
+        }else if (studentInfo>20.10 && studentInfo<=20.20) {
+            console.log("Togrul Rzayev");
+        }else if  (studentInfo>20.20 && studentInfo<=20.30) {
+            console.log("Abbas Abbasov");
+        }else if  (studentInfo>20.30 && studentInfo<=20.40) {
+            console.log("Orxan Ibrahimov");
+        }else if  (studentInfo>20.40 && studentInfo<=20.50) {
+            console.log("Eldar Novruzov");
+        }else {
+            console.log("Emrah Eyyubov");
+        }
+       
+    }
+}
+
