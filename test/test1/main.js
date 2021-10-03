8fccfb6efaedf65d4a2aa6dd01fd25fd623b9924
Arzu <arzu.shahbazli@bk.ru>
2021-10-03 20:51:30 +0400

    test

---
+++ test/test1/main.js
@@ -0,0 +1,125 @@
+//var metn= "Arzu Sahbazli";
+//var netice= metn.search("Sahbazli")
+//document.write(netice);
+//var netice1 = metn.search(/sahbazli/i); bunu sorus
+
+//var melumat = "JavaScript";
+// document.write(melumat);
+// var netice = melumat.slice(4,10);
+//document.write(netice);
+
+//var metn = "JavaScript Programming Language";
+//var netice = metn.substring();
+//document.write(netice);
+
+
+//var metn= "Arzu Sahbazli";
+//var netice = metn.substr(-7,7);
+//document.write(netice);
+
+//var text1 = "1";
+//var text2 = "2";
+//var text3 = text1.concat("", text2);
+//document.write(text3);
+
+//var metn = " Arzu Sahbazli";
+//var netice = metn.repeat(96);
+//document.write(netice);
+
+//var metn = " Arzu Sahbazli";
+//var netice = metn.toString();
+//document.write(netice);
+
+//var x = 123;
+//x.valueOf();            // returns 123 from variable x
+//(123).valueOf();        // returns 123 from literal 123
+//(100 + 23).valueOf();
+//document.write(x);
+
+//var metn = 'Hello World'; 
+
+//var netice = metn.indexOf("World");
+//var netice = metn.indexOf("l",5);
+//document.write(netice);
+
+//var metn = 'JavaScript Java';
+//var netice = metn.LastIndexOf("Java",32);         ---------basa dusmedim--------
+//document.write(netice);                   
+//document.write(metn.fontsize(7));
+
+//var reng = "JavaScript";
+//document.write(reng.fontcolor("green"));
+//document.write(reng.fontcolor("red"));
+
+
+//var metn = "Google";
+//document.write(metn.link("http://google.com"));
+
+//var metn = "hello world";
+//var netice = metn.bold();
+//var netice = metn.italics();
+//var netice =  metn.strike();  ----ustunden xett cekir--
+//var netice =  metn.fixed();    ----font stili teletype--
+//document.write(netice);
+
+//var metn = "Azerbaycan Dovlet Neft ve Senaye Universiteti";
+//var netice = metn.big();
+//var netice = metn.small();
+//document.write(netice);
+
+//var element1 = "C";
+//var element2 = "2";
+//document.write(element1 + element2.sub()); --alt--
+//document.write(element1 + element2.sup()); ---ust--
+
+
+//var metn = "Azerbaycan Dovlet Neft ve Senaye Universiteti";
+//var netice =  metn.toUpperCase();
+//var netice =  metn.toLocaleUpperCase();  -----butun herfler boyuk--
+//var netice =  metn.toLowerCase();
+//var netice =  metn.toLocaleLowerCase();  -----butun herfler kicik--
+//document.write(netice);
+
+//var metn = "JavaScript";
+//document.write(encodeURIComponent(metn));
+//document.write(decodeURIComponent(metn));
+
+//var uri = "my test.asp?name=ståle&car=saab";
+//var res = encodeURI(uri);  ---------basa dusmedim----
+//var dec = decodeURI(enc);
+//var res = enc + "<br>" + dec; 
+//document.write(res);
+
+//var metn = String.fromCharCode(72);
+//document.write(metn);  ---------basa dusmedim----
+
+//var metn = "JavaScript";
+//document.write(metn.charCodeAt(5));
+
+//var metn = "JavaScript superdir";
+//var netice =  metn.startsWith("hello");
+//var netice =metn.endsWith("superdir");
+//document.write(netice);
+
+//var str = "       Hello World!        ";
+ //document.write(alert(str.trim()));
+
+ //var metn = "JavaScript";
+//var netice = metn.includes("Script");
+//var netice = metn.includes("asdasfa"); 
+//document.write(netice);
+
+//var str = "The rain in SPAIN stays mainly in the plain";
+//var res = str.match(/ra/);  --------------bunu sorus----------
+//document.write(res);
+
+//var soz1 = "abc";
+//var soz2 = "ebc";
+//var netice = soz2.localeCompare(soz1);
+//document.write(netice); 
+
+//var str = "How are you doing today?";
+//var res = str.split("r",5);
+//var res = str.split(" ",3);
+//var res = str.split(" ");
+//document.write(res);
