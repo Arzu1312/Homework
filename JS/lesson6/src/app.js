ddd7253d6c45c51b11ac58405beaac647379d192
Arzu <arzu.shahbazli@bk.ru>
2021-10-03 21:12:10 +0400

    lesson6

---
+++ JS/lesson6/src/app.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+function App() {
+    return 
+        <div idName="root">
+          <header className="app-header">
+            <p>
+             <code>Software</code>
+            </p>
+          </header>
+
+          </div>
+    
+}
+
+export default App;
